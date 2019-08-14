53598972c08f371b3cb700664611bb9c19f7516e
Ankhbayar Enkhtaivan <mefbayar@gmail.com>
2019-08-14 18:42:54 +0800

    added y-axis

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Setting up RENDERER, SCENE AND CAMERA
-let renderer = new THREE.WebGLRenderer({antialias: true});
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
-var controls = new THREE.TrackballControls( camera, renderer.domElement );
+var controls = new THREE.TrackballControls(camera, renderer.domElement);
 camera.position.set(35, 10, 0);
 camera.lookAt(0, 0, 0);
 controls.update();
@@ -20,19 +20,19 @@ light.position.set(30, 200, 0).normalize();
 // direction is set in GUI
 scene.add(ambientLight);
 scene.add(light);
-        
+
 // OBJECTS
 
 //This will add a starfield to the background of a scene
 let starsGeometry = new THREE.Geometry();
 
-for (k = 0; k < 10000; k ++) {
-	let star = new THREE.Vector3();
-	star.x = THREE.Math.randFloatSpread(1800);
-	star.y = THREE.Math.randFloatSpread(1800);
-	star.z = THREE.Math.randFloatSpread(1800);
+for (k = 0; k < 10000; k++) {
+  let star = new THREE.Vector3();
+  star.x = THREE.Math.randFloatSpread(1800);
+  star.y = THREE.Math.randFloatSpread(1800);
+  star.z = THREE.Math.randFloatSpread(1800);
 
-	starsGeometry.vertices.push(star);
+  starsGeometry.vertices.push(star);
 }
 
 let starsMaterial = new THREE.PointsMaterial({ color: 0xFFFFFF });
@@ -41,12 +41,12 @@ scene.add(starField);
 
 // Adding sun
 let geometry_sun = new THREE.SphereGeometry(7, 24, 24);
-let material_sun = new THREE.MeshPhongMaterial({color: 0xFFC303});
+let material_sun = new THREE.MeshPhongMaterial({ color: 0xFFC303 });
 let sun = new THREE.Mesh(geometry_sun, material_sun);
 
 // Earth - the only planet in the beggining
 let geometry_earth = new THREE.SphereGeometry(1, 24, 24);
-let material_earth = new THREE.MeshPhongMaterial({color: 0x00AAFA});
+let material_earth = new THREE.MeshPhongMaterial({ color: 0x00AAFA });
 let earth = new THREE.Mesh(geometry_earth, material_earth);
 
 scene.add(sun);
@@ -60,8 +60,13 @@ function makeCircle(r) {
   let points = [];
   let incr = Math.random() * (0.1 - 0.005) + 0.005;
   let alpha = Math.random() * 2 * Math.PI;
+  let gamma = Math.random() * 2 * Math.PI;
   for (beta = 0; beta < 2 * Math.PI; beta += incr) {
-    points.push([r*Math.cos(alpha + beta), r*Math.sin(alpha + beta)]);
+    points.push([
+      r * Math.cos(alpha + beta) * Math.sin(gamma),
+      r * Math.sin(alpha + beta),
+      r * Math.cos(alpha + beta) * Math.cos(gamma)
+    ]);
   }
   return points;
 }
@@ -80,7 +85,8 @@ function animate() {
   for (let j = 0; j < planets.length; j++) {
     let len = paths[j].length;
     planets[j].position.x = paths[j][i % len][0];
-    planets[j].position.z = paths[j][i % len][1];
+    planets[j].position.y = paths[j][i % len][1];
+    planets[j].position.z = paths[j][i % len][2];
   } i++;
 
   renderer.render(scene, camera);
@@ -104,7 +110,7 @@ let r = 25;
 document.getElementById('add-planet').onclick = function () {
   let color = Math.random() * 0xFFC303;
   let geometry = new THREE.SphereGeometry(Math.random() * 2 + 0.5, 24, 24);
-  let material = new THREE.MeshPhongMaterial({color});
+  let material = new THREE.MeshPhongMaterial({ color });
   let planet = new THREE.Mesh(geometry, material);
   planets.push(planet);
   paths.push(makeCircle(r));
